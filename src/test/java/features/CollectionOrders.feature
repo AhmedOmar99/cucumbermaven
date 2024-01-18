baadfe9d168d41a93b2e48294235ae107d8dbe7f
ahmedomar99 <ahmed.dahab@maxab.io>
2024-01-18 18:17:49 +0200

    test

--- src/test/java/features/CollectionOrders.feature
+++ src/test/java/features/CollectionOrders.feature
@@ -2,5 +2,5 @@
 Feature: Collection orders
   Scenario: Change partial flag of CO
     Given User is authorized with credentials
-    When user send the request to change the value of partial flag
-    Then partial flag should changed
+    When user sends the request to change the value of partial flag
+    Then partial flag should be changed
--- src/test/java/steps/CollectionOrders.java
+++ src/test/java/steps/CollectionOrders.java
@@ -4,37 +4,37 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
-import org.openqa.selenium.WebDriver;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import static steps.KeycloakRestAssuredSSOLoginAutomation.performSSOLogin;
 
 public class CollectionOrders {
-    static String accessToken = performSSOLogin();
-    String portalBaseUrl = "https://emoney-portal-fintech-dev.maxdev.org/emoney/api/v1";
+
+    private static final String PORTAL_BASE_URL = "https://emoney-portal-fintech-dev.maxdev.org/emoney/api/v1";
+    private static String accessToken = performSSOLogin();
+    int coID = 15559;
 
     @Given("User is authorized with credentials")
     public static String checkUserIsAuthorized() {
-        System.out.println("Bearer "+accessToken);
+        System.out.println("Bearer " + accessToken);
         return accessToken;
     }
 
-    @When("user send the request to change the value of partial flag")
+    @When("user sends the request to change the value of partial flag")
     public void changePartialFlagOfCO() {
-        String accessToken = performSSOLogin();
-        System.out.println("Access token: " + accessToken);
+
         given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(ContentType.JSON)
                 .body("{\n" +
-                        "    \"newPartialFlag\": true,\n" +
+                        "    \"newPartialFlag\": false,\n" +
                         "    \"newDate\": null,\n" +
                         "    \"collectionOrderIds\": [\n" +
-                        "        15534\n" +
+                              coID +
                         "    ]\n" +
                         "}")
-                .patch(portalBaseUrl+"/collection-orders/update-collection-orders")
+                .patch(PORTAL_BASE_URL + "/collection-orders/update-collection-orders")
                 .then()
                 .statusCode(200)
                 .assertThat()
@@ -42,26 +42,22 @@ public class CollectionOrders {
                 .log().all();
     }
 
-    @Then("partial flag should changed")
+    @Then("partial flag should be changed")
     public void checkTheValueOfPartialFlagChanged() {
-        String accessToken = performSSOLogin();
-        System.out.println("Access token: " + accessToken);
         given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(ContentType.JSON)
                 .body("{\n" +
-                        "    \"newPartialFlag\": true,\n" +
-                        "    \"newDate\": null,\n" +
-                        "    \"collectionOrderIds\": [\n" +
-                        "        15534\n" +
-                        "    ]\n" +
+                        "  \"collectionOrderIds\": [\n" +
+                        coID+
+                        "  ]\n" +
                         "}")
-                .post(portalBaseUrl+"/collection-orders")
+                .post(PORTAL_BASE_URL + "/collection-orders")
                 .then()
                 .statusCode(200)
                 .assertThat()
                 .body("$", hasSize(greaterThanOrEqualTo(1)))
-                .body("[0].canCollectPartially", equalTo(true))
+                .body("[0].canCollectPartially", equalTo(false))
                 .log().all();
     }
 }
